2775e66ab5f493094c9a16bd4f1c0adb540889bf
Benzi <benzi.villaruel@gmail.com>
2024-12-02 18:21:53 -0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+import java.util.Properties
+
+val file = file("apikey.properties")
+val properties = Properties()
+properties.load(file.inputStream())
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.wanderer"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.wanderer"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "GOOGLE_PLACES_API_KEY", "\"${properties.getProperty("GOOGLE_PLACES_API_KEY")}\"")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation ("androidx.room:room-ktx:2.4.2")
+    implementation ("androidx.room:room-runtime:2.4.2")
+    annotationProcessor ("androidx.room:room-compiler:2.4.2")
+    kapt ("androidx.room:room-compiler:2.4.2")
+    implementation ("androidx.fragment:fragment-ktx:1.5.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
+    implementation ("androidx.core:core-ktx:1.8.0")
+    implementation ("androidx.appcompat:appcompat:1.4.2")
+    implementation ("com.google.android.material:material:1.6.1")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.codepath.libraries:asynchttpclient:2.2.0")
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+    implementation ("androidx.recyclerview:recyclerview-selection:1.1.0")
+    implementation ("com.github.bumptech.glide:glide:4.13.2")
+    implementation ("com.google.code.gson:gson:2.9.0")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.13.2")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
